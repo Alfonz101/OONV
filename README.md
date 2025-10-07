f4615e5d12c49330a4992a73f19818f7698dadbc
Rob <roberthomaseaborn@gmail.com>
2025-10-07 10:03:09 +0200

    builder and singleton method to-be-done

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## Setting up a project
 
 `dotnet new console -n NAME --use-program-main`
+
+[Sources](https://drive.google.com/drive/folders/1NQClVCcfn_PbDOZ88xoOzBqGmtejn9zc)
---
+++ Singleton/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Singleton;
+
+public sealed class Singleton
+{
+    
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
